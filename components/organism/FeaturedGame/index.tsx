46d51fa26c5edc27d3b44cc356d3d6e4bca71695
ariaschecter <aria.maulana.m@gmail.com>
2023-09-02 12:08:25 +0700

    [41] Membuat API data types

--- components/organism/FeaturedGame/index.tsx
+++ components/organism/FeaturedGame/index.tsx
@@ -1,6 +1,7 @@
 import GameItem from '@/components/molecules/GameItem';
 import React, { useCallback, useEffect, useState } from 'react';
 import { getFeaturedGame } from '@/services/player';
+import { GameItemTypes } from '@/services/data-types';
 
 export default function FeaturedGame() {
   const [gameList, setGameList] = useState([]);
@@ -13,6 +14,8 @@ export default function FeaturedGame() {
   useEffect(() => {
     getFeatureGameList();
   }, []);
+
+  const API_IMG = process.env.NEXT_PUBLIC_IMG;
   return (
     <section className="featured-game pt-50 pb-50">
       <div className="container-fluid">
@@ -24,14 +27,18 @@ export default function FeaturedGame() {
           className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
           data-aos="fade-up"
         >
-          {gameList.map(item => {
+          {gameList.map((item: GameItemTypes) => {
             return (
-              <GameItem key={item._id} title={item.name} category={item.category.name} thumbnail={`http://localhost:3000/uploads/${item.thumbnail}`} />
-            )
+              <GameItem
+                key={item._id}
+                title={item.name}
+                category={item.category.name}
+                thumbnail={`${API_IMG}/${item.thumbnail}`}
+              />
+            );
           })}
-
         </div>
       </div>
     </section>
-  )
+  );
 }
