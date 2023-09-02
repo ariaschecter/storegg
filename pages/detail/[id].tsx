8b0f152658198cf781f1b947eb288838a6b3885a
ariaschecter <aria.maulana.m@gmail.com>
2023-09-02 12:57:17 +0700

    [43] Integrasi Voucher Detail

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -2,18 +2,32 @@ import Footer from '@/components/organism/Footer';
 import Navbar from '@/components/organism/Navbar';
 import TopUpForm from '@/components/organism/TopUpForm';
 import TopUpItem from '@/components/organism/TopUpItem';
+import { getDetailVoucher } from '@/services/player';
 import { useRouter } from 'next/router';
 
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 export default function Detail() {
   const { query, isReady } = useRouter();
+  const [dataItem, setDataItem] = useState({
+    name: '',
+    thumbnail: '',
+    category: {
+      name: '',
+    }
+  });
+
+  const getVoucherDetailAPI = useCallback( async(id) => {
+    const data = await getDetailVoucher(id);
+    setDataItem(data);
+    console.log(data);
+  }, []);
 
   useEffect(() => {
     if (isReady) {
-      console.log('router sudah tersedia', query.id);
+      getVoucherDetailAPI(query.id);
     } else {
-      console.log('router tidak tersedia');
+      // console.log('router tidak tersedia');
     }
   }, [isReady]);
   return (
@@ -31,10 +45,10 @@ export default function Detail() {
           </div>
           <div className="row">
             <div className="col-xl-3 col-lg-4 col-md-5 pb-30 pb-md-0 pe-md-25 text-md-start">
-              <TopUpItem type="mobile" />
+              <TopUpItem type="mobile" data={dataItem} />
             </div>
             <div className="col-xl-9 col-lg-8 col-md-7 ps-md-25">
-              <TopUpItem type="desktop" />
+              <TopUpItem data={dataItem} type="desktop" />
               <hr />
               <TopUpForm />
             </div>
